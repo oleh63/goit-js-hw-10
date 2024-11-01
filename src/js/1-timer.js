1f4b6f6c0825b777006b5c52ec2d739f7f47df22
Oleh Skrypets <olehskrupets@gmail.com>
2024-11-01 15:10:53 +0200

    update-2

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,8 +1,7 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
-// import "flatpickr/dist/themes/dark.css";
-// import iziToast from "izitoast";
-// import "izitoast/dist/css/iziToast.min.css";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 
 let userSelectedDate = null;
 const startButton = document.querySelector('[data-start]');
@@ -14,11 +13,21 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+      const selectedDate = selectedDates[0];
+      if (selectedDate <= new Date()) {
+          iziToast.error({
+              title: 'Error',
+              message: 'Please choose a date in the future',
+          });
+          startButton.disabled = true;
+      } else {
+          userSelectedDate = selectedDate;
+          startButton.disabled = false;
+      }
   },
 };
 
-flatpickr("#datatime-picker", options);
+flatpickr("#datetime-picker", options);
 
 
 
