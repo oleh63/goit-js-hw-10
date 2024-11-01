2046dd31871de93174d9c2f37c87177c8027c6b0
oleh63 <167691341+oleh63@users.noreply.github.com>
2024-11-01 11:53:58 +0000

    Deploying to gh-pages from @ oleh63/goit-js-hw-10@88c9341e5985e78db465dd9aef7be8e8b32e16fa 🚀

---
+++ 1-timer.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{f as e}from"./assets/vendor-EyZmBGcZ.js";const o=document.querySelector("[data-start]");o.disabled=!0;const r={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(t){console.log(t[0])}};e("#datatime-picker",r);
+//# sourceMappingURL=1-timer.js.map
