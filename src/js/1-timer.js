9e762b9ab1f41991ad6735f22a3885ec110fd062
Oleh Skrypets <olehskrupets@gmail.com>
2024-11-01 15:45:26 +0200

    update-3

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -29,6 +29,47 @@ const options = {
 
 flatpickr("#datetime-picker", options);
 
+startButton.addEventListener('click', () => {
+    startButton.disabled = true;
+    const timerId = setInterval(() => {
+        const currentTime = new Date();
+        const deltaTime = userSelectedDate - currentTime;
+
+        if (deltaTime <= 0) {
+            clearInterval(timerId);
+            updateTimerDisplay(0, 0, 0, 0);
+            return;
+        }
+        const { days, hours, minutes, seconds } = convertMs(deltaTime);
+        updateTimerDisplay(days, hours, minutes, seconds);
+    }, 1000);
+});
+
+function convertMs(ms) {
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const days = Math.floor(ms / day);
+    const hours = Math.floor((ms % day) / hour);
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+    return { days, hours, minutes, seconds };
+}
+
+function updateTimerDisplay(days, hours, minutes, seconds) {
+    document.querySelector('[data-days]').textContent = addLeadingZero(days);
+    document.querySelector('[data-hours]').textContent = addLeadingZero(hours);
+    document.querySelector('[data-minutes]').textContent = addLeadingZero(minutes);
+    document.querySelector('[data-seconds]').textContent = addLeadingZero(seconds);   
+}
+
+function addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+}
+
 
 
 
