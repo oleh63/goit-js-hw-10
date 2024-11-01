b8f53413618606007668d65db152ff5d88af5354
Oleh Skrypets <olehskrupets@gmail.com>
2024-11-01 16:17:27 +0200

    update-4

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,6 +1,5 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
-import "flatpickr/dist/themes/dark.css"
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
