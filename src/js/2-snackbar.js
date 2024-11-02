31e9a40aaed84cac5b3e15976ddfa61c0e7cc89f
Oleh Skrypets <olehskrupets@gmail.com>
2024-11-02 14:07:06 +0200

    update-7

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,33 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (state === 'fulfilled') {
+                resolve(delay);
+            } else {
+                reject(delay);
+            }
+        }, delay);
+    });
+    promise
+        .then((delay) => {
+            iziToast.success({
+                title: 'Success',
+                message: `✅ Fulfieled in ${delay}ms`,
+                position: 'topRight',
+            });
+        })
+        .catch((delay) => {
+            iziToast.error({
+                title: 'Error',
+                message: `❌ Rejected promise in ${delay}ms`,
+                position: 'topRight',
+            });
+        });
+});
