1bb53af77241bfc584c18464b281a3c91b82320c
Oleh Skrypets <olehskrupets@gmail.com>
2024-11-02 16:22:27 +0200

    update-finish

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -6,6 +6,9 @@ const form = document.querySelector('.form');
 form.addEventListener('submit', (event) => {
     event.preventDefault();
 
+    const delay = Number(event.target.elements.delay.value);
+    const state = event.target.elements.state.value;
+
     const promise = new Promise((resolve, reject) => {
         setTimeout(() => {
             if (state === 'fulfilled') {
