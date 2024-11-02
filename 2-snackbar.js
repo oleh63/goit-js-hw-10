a8778bc5b2d847ac776e6a63da7996daa0851fdf
oleh63 <167691341+oleh63@users.noreply.github.com>
2024-11-02 12:07:34 +0000

    Deploying to gh-pages from @ oleh63/goit-js-hw-10@31e9a40aaed84cac5b3e15976ddfa61c0e7cc89f 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{i as t}from"./assets/vendor-BbbuE1sJ.js";const o=document.querySelector(".form");o.addEventListener("submit",i=>{i.preventDefault(),new Promise((e,s)=>{setTimeout(()=>{state==="fulfilled"?e(delay):s(delay)},delay)}).then(e=>{t.success({title:"Success",message:`✅ Fulfieled in ${e}ms`,position:"topRight"})}).catch(e=>{t.error({title:"Error",message:`❌ Rejected promise in ${e}ms`,position:"topRight"})})});
+//# sourceMappingURL=2-snackbar.js.map
